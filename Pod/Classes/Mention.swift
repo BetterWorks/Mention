c8d1f81b923256dcec83b30f4fc00d550fadf893
Connor Smith <cmds4410@gmail.com>
2015-12-09 15:59:23 -0800

    allow typing before a mention at location 0 in the string

--- Pod/Classes/Mention.swift
+++ Pod/Classes/Mention.swift
@@ -483,9 +483,9 @@ public class MentionComposer<T: UIView where T: ComposableAttributedTextContaini
         var rangeToUpdate: NSRange?
         view?.m_attributedText?.enumerateAttribute(NSForegroundColorAttributeName, inRange: NSRange(location: 0, length: updatedTextLength), options: NSAttributedStringEnumerationOptions(rawValue: 0)) { (value, range, stop) -> Void in
             guard let color = value as? UIColor where color == MentionColor else { return }
-            guard let attributes = self.view?.m_attributedText?.attributesAtIndex(range.location, effectiveRange: nil) where !attributes.keys.contains(MentionAttributes.Encoded) else { return }
-
-            rangeToUpdate = range
+            var effectiveRange = NSRange(location: 0, length: 20)
+            guard let attributes = self.view?.m_attributedText?.attributesAtIndex(range.location, effectiveRange: &effectiveRange) where !attributes.keys.contains(MentionAttributes.Encoded) else { return }
+            rangeToUpdate = effectiveRange
             stop.memory = true
         }
 
